7bd86cb5dedb0b4c81d01300477274c9f2aff5ce
mpdillingerOSU <mpdillinger@gmail.com>
2025-04-15 16:00:22 -0500

    add welcome page image

--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -6,6 +6,9 @@ function WelcomePage() {
             <div className="game-title-container">
                 <img className="game-title" src={require("../images/game-title.png")} alt="game title" />
             </div>
+            <div className="welcome-page-img-container">
+                <img className="welcome-page-img" src={require("../images/welcome-page-img.png")} alt="welcome page image" />
+            </div>
         </div>
     );
 }
