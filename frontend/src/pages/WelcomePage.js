98228dbaf2715db7e3647ae256d73d4b9475db22
mpdillingerOSU <mpdillinger@gmail.com>
2025-04-15 15:46:03 -0500

    add game title to welcome page

--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function WelcomePage() {
     return (
-        <div />
+        <div>
+            <div className="game-title-container">
+                <img className="game-title" src={require("../images/game-title.png")} alt="game title" />
+            </div>
+        </div>
     );
 }
 
